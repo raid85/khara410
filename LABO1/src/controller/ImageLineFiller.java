d6d0fd45474c78a96b2f40f7ffc15b6f2a3da8f5
chebli.r@gmail.com <chebli.r@gmail.com@2725efa7-3996-e4a8-26e7-0c49c0770588>
2012-10-16 19:49:33 +0000

    BOUNDRY FILL IN PROGRESS

--- LABO1/src/controller/ImageLineFiller.java
+++ LABO1/src/controller/ImageLineFiller.java
@@ -75,7 +75,7 @@ public class ImageLineFiller extends AbstractTransformer {
 					currentImage.beginPixelUpdate();
 					//horizontalLineFill(ptTransformed);
 					if (floodFill){floodFill(ptTransformed);}
-					else boundaryFill(ptTransformed);
+					else boundFill(ptTransformed);
 				}
 				currentImage.endPixelUpdate();											 	
 				return true;
@@ -83,55 +83,149 @@ public class ImageLineFiller extends AbstractTransformer {
 		}
 
 		return false;}
-	private void boundaryFill(Point ptClicked) {
-		// TODO Auto-generated method stub
 
-	}
+	public void boundFill(Point ptClicked){ 
 
-	private void floodFill(Point ptClicked) {
-	
+		int[] rgb = new int[3];
+		rgb = hsv2rgb((float)hueThreshold/180,(float)saturationThreshold/255,(float)valueThreshold/255);
+		System.out.println("RGB : "+rgb[0]+"..."+rgb[1]+"..."+rgb[2]);
+		int rmax = borderColor.getRed()+rgb[0];
+		int rmin = borderColor.getRed()-rgb[0];		
+		int gmax = borderColor.getGreen()+rgb[1];
+		int gmin = borderColor.getGreen()-rgb[1];
+		int bmax = borderColor.getBlue()+rgb[2];
+		int bmin = borderColor.getBlue()-rgb[2];	
+		if (rmin<0) rmin= 0 ;
+		if (gmin<0) gmin= 0 ;
+		if (bmin<0) bmin= 0 ;
+		if (rmax>255) rmax= 255 ;
+		if (gmax>255) gmax= 255 ;
+		if (bmax>255) bmax= 255 ;
+		System.out.println("COLOR RANGE FOR R : "+rmin+" < "+borderColor.getRed()+" > "+rmax);
+		System.out.println("COLOR RANGE FOR G : "+gmin+" < "+borderColor.getGreen()+" > "+gmax);
+		System.out.println("COLOR RANGE FOR B : "+bmin+" < "+borderColor.getBlue()+" > "+bmax);
+
 		Stack stack = new Stack();
 		stack.push(ptClicked);
-		currentColor = currentImage.getPixel(ptClicked.x, ptClicked.y);
+
 		while (!stack.empty()) {
-			Point current = (Point)stack.pop();
+			Point current = (Point)stack.pop();				
 			
-			
-			if (0 <= current.x && current.x < currentImage.getImageWidth() && 0 <= current.y && current.y < currentImage.getImageHeight()&&
+
+			if (0 <= current.x && current.x < currentImage.getImageWidth() && 0 < current.y && current.y < (currentImage.getImageHeight()-1)&&
 					!currentImage.getPixel(current.x, current.y).equals(fillColor)) {
 				currentImage.setPixel(current.x, current.y, fillColor);
 				
-				// Next points to fill.
-				if(current.x>0 && current.x< currentImage.getImageWidth() && current.y < (currentImage.getImageHeight()-1)&& current.y >0 ){
+				// Next points to fill.			
+				if(!((currentImage.getPixel(current.x-1, current.y).getRed()<=rmax) &&(currentImage.getPixel(current.x-1, current.y).getRed()>=rmin))
+						||!((currentImage.getPixel(current.x-1, current.y).getGreen()<=gmax)&& (currentImage.getPixel(current.x-1, current.y).getGreen()>=gmin))
+						||!((currentImage.getPixel(current.x-1, current.y).getBlue()<=bmax) && (currentImage.getPixel(current.x-1, current.y).getBlue()>=bmin))){
+					
+					Point nextLeft = new Point(current.x-1, current.y);
+					
+					stack.push(nextLeft);
+				}
+				if(!((currentImage.getPixel(current.x+1, current.y).getRed()<=rmax) && (currentImage.getPixel(current.x+1, current.y).getRed()>=rmin))
+						||!((currentImage.getPixel(current.x+1, current.y).getGreen()<=gmax) &&(currentImage.getPixel(current.x+1, current.y).getGreen()>=gmin))
+						||!((currentImage.getPixel(current.x+1, current.y).getBlue()<=bmax) &&(currentImage.getPixel(current.x+1, current.y).getBlue()>=bmin))){
+					Point nextRight = new Point(current.x+1, current.y);
+					stack.push(nextRight);
+				}
+				if(!((currentImage.getPixel(current.x, current.y-1).getRed()<=rmax) &&(currentImage.getPixel(current.x, current.y-1).getRed()>=rmin))
+						||!((currentImage.getPixel(current.x, current.y-1).getGreen()<=gmax) &&(currentImage.getPixel(current.x, current.y-1).getGreen()>=gmin))
+						||!((currentImage.getPixel(current.x, current.y-1).getBlue()<=bmax) &&(currentImage.getPixel(current.x, current.y-1).getBlue()>=bmin))){
+					Point nextUp = new Point (current.x,current.y-1);
+					stack.push(nextUp);
+				}
+				if(!((currentImage.getPixel(current.x, current.y+1).getRed()<=rmax) &&(currentImage.getPixel(current.x, current.y+1).getRed()>=rmin))
+						||!((currentImage.getPixel(current.x, current.y+1).getGreen()<=gmax) &&(currentImage.getPixel(current.x, current.y+1).getGreen()>=gmin))
+						||!((currentImage.getPixel(current.x, current.y+1).getBlue()<=bmax) &&(currentImage.getPixel(current.x, current.y+1).getBlue()>=bmin))){
+					Point nextDown = new Point (current.x,current.y+1);
+					stack.push(nextDown);
+				}
+			}
+		}
+	}
+
+	private int [] hsv2rgb (float h, float s, float v) {
+
+		// h,s,v in [0,1]
+		float rr = 0, gg = 0, bb = 0;
+		float hh = (6 * h) % 6;                 
+		int   c1 = (int) hh;                     
+		float c2 = hh - c1;
+		float x = (1 - s) * v;
+		float y = (1 - (s * c2)) * v;
+		float z = (1 - (s * (1 - c2))) * v;	
+		switch (c1) {
+		case 0: rr=v; gg=z; bb=x; break;
+		case 1: rr=y; gg=v; bb=x; break;
+		case 2: rr=x; gg=v; bb=z; break;
+		case 3: rr=x; gg=y; bb=v; break;
+		case 4: rr=z; gg=x; bb=v; break;
+		case 5: rr=v; gg=x; bb=y; break;
+		}
+		int N = 256;
+		int r = Math.min(Math.round(rr*N),N-1);
+		int g = Math.min(Math.round(gg*N),N-1);
+		int b = Math.min(Math.round(bb*N),N-1);
+
+
+
+		int [] rgb = new int[3];
+		rgb[0] = r ;
+		rgb[1] = g;
+		rgb[2] = b ;
+		return rgb;
+	}
+
+
+	private void floodFill(Point ptClicked) {
+
+		Stack stack = new Stack();
+		stack.push(ptClicked);
+		currentColor = currentImage.getPixel(ptClicked.x, ptClicked.y);
+
+
+		while (!stack.empty()) {
+			Point current = (Point)stack.pop();
+
+
+
+			if (0 <= current.x && current.x < currentImage.getImageWidth() && 0 < current.y && current.y < (currentImage.getImageHeight()-1)&&
+					!currentImage.getPixel(current.x, current.y).equals(fillColor)) {
+				currentImage.setPixel(current.x, current.y, fillColor);
+
+				// Next points to fill.			
 				if(currentImage.getPixel(current.x-1, current.y).equals(currentColor)){
 					Point nextLeft = new Point(current.x-1, current.y);
 					stack.push(nextLeft);
 				}
 				if(currentImage.getPixel(current.x+1, current.y).equals(currentColor)){
-				Point nextRight = new Point(current.x+1, current.y);
-				stack.push(nextRight);
-				}
-				if(currentImage.getPixel(current.x, current.y+1).equals(currentColor)){
-				Point nextUp = new Point (current.x,current.y+1);
-				stack.push(nextUp);
+					Point nextRight = new Point(current.x+1, current.y);
+					stack.push(nextRight);
 				}
 				if(currentImage.getPixel(current.x, current.y-1).equals(currentColor)){
-				Point nextDown = new Point (current.x,current.y-1);
-				stack.push(nextDown);
-				}	
+					Point nextUp = new Point (current.x,current.y-1);
+					stack.push(nextUp);
 				}
-				
-				
+				if(currentImage.getPixel(current.x, current.y+1).equals(currentColor)){
+					Point nextDown = new Point (current.x,current.y+1);
+					stack.push(nextDown);
+				}		
+
+
+
 			}
 		}
-		
-//		floodFill(x, y, interiorColor, newColor)
-//		if (getPixel(x,y) == interiorColor) 
-//		setpixel(x,y,newColor) 
-//		floodFill(x+1,y,interiorColor, newColor)
-//		floodFill(x-1,y,interiorColor, newColor) 
-//		floodFill(x,y+1,interiorColor, newColor)
-//		floodFill(x,y-1,interiorColor, newColor)
+
+		//		floodFill(x, y, interiorColor, newColor)
+		//		if (getPixel(x,y) == interiorColor) 
+		//		setpixel(x,y,newColor) 
+		//		floodFill(x+1,y,interiorColor, newColor)
+		//		floodFill(x-1,y,interiorColor, newColor) 
+		//		floodFill(x,y+1,interiorColor, newColor)
+		//		floodFill(x,y-1,interiorColor, newColor)
 
 	}
 
